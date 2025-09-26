022105178bacd801337c1876be8de71952a5c7d6
cyprianabincha-lang <cyprianabincha@gmail.com>
2025-09-26 12:26:23 +0300

    Add files via upload

---
+++ assignment 1a.c
@@ -0,0 +1,18 @@
+/*
+Name: Cyprian Abincha 
+Reg no:PA106/G/28791/25
+Description: program to display all units
+*/
+
+#include<stdio.h> //preprocessor directive: scanf(),printf()
+//main function 
+int main() {
+    printf("1.Mathematics for science\n");
+    printf("2.Fundamentals of ICT\n");
+    printf("3Computer systems and organization\n");
+    printf("4.communication skills\n");
+    printf("5.Fundamentals of system analysis and design\n");
+    printf("6.programming and problem solving\n");
+    printf("7.Discrete mathematics\n");
+    return 0;
+}
