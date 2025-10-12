6083b69e3399a269491d0d82d582c31ae5109bff
cyprianabincha-lang <cyprianabincha@gmail.com>
2025-10-12 10:39:33 +0300

    Add files via upload

---
+++ numbers for loop.c
@@ -0,0 +1,18 @@
+/*
+Name cyprian Abincha 
+Reg PA106/G/28791/25
+*/
+
+#include<stdio.h>
+
+int main(){
+    
+    int i;
+    for(i=50;i<=100;i++){
+    printf("%d\n",i);
+    }
+
+
+return 0;
+}
+
