9c7c8839323c1bff4140dcdd1203d9cc01ff1c14
cyprianabincha-lang <cyprianabincha@gmail.com>
2025-10-19 22:50:44 +0300

    Add files via upload

---
+++ Electricity bill.c
@@ -0,0 +1,40 @@
+/*Name;Cyprian Abincha
+Reg:PA106/G/28791/25
+*/
+
+#include <stdio.h>
+
+// Function prototype
+float calculateBill(int units);
+
+int main() {
+    int units;
+    float total;
+
+    // Input from user
+    printf("Enter number of units consumed: ");
+    scanf("%d", &units);
+
+    // Function call
+    total = calculateBill(units);
+
+    // Output result
+    printf("Total electricity bill: %.2f\n", total);
+
+    return 0;
+}
+
+// Function definition
+float calculateBill(int units) {
+    float bill = 0;
+
+    if (units <= 100) {
+        bill = units * 10;
+    } else if (units <= 200) {
+        bill = (100 * 10) + ((units - 100) * 15);
+    } else {
+        bill = (100 * 10) + (100 * 15) + ((units - 200) * 20);
+    }
+
+    return bill;
+}
