8bad9624baf8dcfb896cc92d03a29aeb7799da49
cyprianabincha-lang <cyprianabincha@gmail.com>
2025-10-19 09:48:20 +0300

    Add files via upload

---
+++ degree conversion.c
@@ -0,0 +1,33 @@
+/*Name  Cyprian Abincha
+Reg PA106/G/28791/25
+*/
+
+
+
+
+#include <stdio.h>
+
+// Function prototype
+float c(float f);
+
+int main()
+{
+    float degree_celsius, fahrenheit;
+
+    printf("Enter the Fahrenheit temperature:\n");
+    scanf("%f", &fahrenheit);
+
+    degree_celsius = c(fahrenheit);
+
+    printf("The Celsius temperature is = %f\n", degree_celsius);
+
+    return 0;
+}
+
+// Function definition
+float c(float f)
+{
+    float celsius;
+    celsius = (f - 32) * 5 / 9;
+    return celsius;
+}
