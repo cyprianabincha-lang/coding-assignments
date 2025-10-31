20a39f62b4ff77ee113e8d6cae599b61c88e89c1
cyprianabincha-lang <cyprianabincha@gmail.com>
2025-10-31 07:05:59 +0300

    Add files via upload

---
+++ branches_rooms.c
@@ -0,0 +1,48 @@
+/*PA106/G/28791/25
+Cyprian Abincha 
+program to calculate total number of occupied rooms accrooss three branches*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int chain[3][5][10];
+    int branch, floor, room;
+    int totalOccupied = 0;
+
+    srand(time(0)); 
+
+    
+    for (branch = 0; branch < 3; branch++)
+    {
+        for (floor = 0; floor < 5; floor++)
+        {
+            for (room = 0; room < 10; room++)
+            {
+                chain[branch][floor][room] = rand() % 2; 
+            }
+        }
+    }
+
+    
+    for (branch = 0; branch < 3; branch++)
+    {
+        for (floor = 0; floor < 5; floor++)
+        {
+            for (room = 0; room < 10; room++)
+            {
+                if (chain[branch][floor][room] == 1)
+                {
+                    totalOccupied++;
+                }
+            }
+        }
+    }
+
+    printf("Total number of occupied rooms across all branches: %d\n", totalOccupied);
+
+    return 0;
+}
